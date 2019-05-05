6d395da2c43c1a1da181c057fefe47ac2aed8df0
Nikolay Shvets <47063312+NikolayShvets@users.noreply.github.com>
2019-05-05 14:52:14 +0300

    Add files via upload

--- Wampoos.cpp
+++ Wampoos.cpp
@@ -28,12 +28,6 @@ enum ch_weapon {DAGGER = 0, DDAGGER, SWORD, DSWORD, AXE, DAXE, MACE, THSWORD, TH
                                                                         //топор, два топора, булава, двуручный меч, 
 																		//двуручный топор, лук, щит, гримуар, волшебный посох, 
                                                                         //волшебная палочка, свиток заклинаний, безоружный)
-/*ch_weapon weapon;
-ch_armour armour;
-enum ch_dressed_armour {HELMET = 0, BODY, GLOVES, TROUSERS, BOOTS}; // надетая одежда (шлем, тело, перчатки, штаны, сапоги)
-enum ch_dressed_weapons {LEFT_HAND = 0, RIGHT_HAND}; // надетое оружие (левая рука, правая рука)
-ch_dressed_armour dressed_armour;
-ch_dressed_weapons dressed_weapons;*/
 
 struct  Character
 {
@@ -56,32 +50,38 @@ Character character;
 
 void Setup(); //функция подгрузки стартовых параметров игры (запускается один раз при старте)
 void DrawMap(char skin, string map_name, move_var ch_move); // функция отрисовки карты (вызывается каждый раз при перемещении персонажа)
+//принимает на вход: skin -  символ отображения персонажа, map_name - название файла карты ("С:\...\location.txt"), ch_move - направление 
+//движения персонажа из перечисления move_var (STOP, UP, LEFT, RIGHT, DOWN)
 void Input(); // функция обработки пользовательского ввода
 void Logic(); // логика игры
 
 int main()
 {
-	Setup();
-	while (!gameOver)
+	Setup(); //подгружаем стартовые параметры игры
+	while (!gameOver) //пока игра не окончена
 	{
-		Input();
-		Logic();
+		Input(); //обрабатываем пользовательский ввод
+		Logic(); //отрабатываем логику игры
 	}
-	if (gameOver)
+	if (gameOver) //если игра окночена (gameOver == true)
 	{
-		system("cls");
-		cout << "Game over!";
+		system("cls"); // чистим экран
+		cout << "Game over!"; // выводим надпись Game over!
 	}
 	return 0;
 }
 void Setup()
 {
+	/*чистим экран, вводим имя, выбираем расу, класс, ... , в зависимости от выбора оператором switch case выводим соответсвующую
+	информацию на экран. В случае с полом, кроме сообщения на экран, определяем симовол отображения персонажа*/
 	system("cls");
 	int temp{ 0 };
 	cout << "Character editor" << endl;
+	// имя
 	cout << "Input the name of your character: " << endl;
 	getline(cin, character._name);
 	cout << "The name of your character "<< character._name << endl;
+	//раса
 	cout << "Select the race of your character: \n    0 - Human\n    1 - Elf\n    2 - Dwarf" << endl;
 	cin >> temp;
 	switch (temp)
@@ -91,14 +91,16 @@ void Setup()
 	case 2: character._race = DWARF; cout << "Character race is Dwarf" <<endl; break;
 	default: character._race = HUMAN; cout << "Character race is Human" <<endl; break;
 	}
+	//пол
 	cout << "Select the gender of your character: \n    0 - Male\n    1 - Female" << endl;
 	cin >> temp;
 	switch (temp)
 	{
-	default: character._gender = MALE; cout << "The gender of your character male" << endl; character.ch_skin = 'T'; break;
+	default: character._gender = MALE; cout << "The gender of your character male" << endl; character.ch_skin = 'T'; break; // мужской - Т, женский - Y
 	case 0: character._gender = MALE; cout << "The gender of your character male" << endl; character.ch_skin = 'T'; break;
 	case 1: character._gender = FEMALE; cout << "The gender of your character female" << endl; character.ch_skin = 'Y'; break;
 	}
+	//класс
 	cout << "Select the class of your character: \n    0 - Wizard\n    1 - Assasin\n    2 - Prist\n    3 - Warrior\n    4 - Necromancer" << endl;
 	cin >> temp;
 	switch (temp)
@@ -110,45 +112,55 @@ void Setup()
 	case 3: character._class = WARRIOR; cout << "The class of your character Warrior" << endl; break;
 	case 4: character._class = NECROMANCER; cout << "The class of your character Necromancer" << endl; break;
 	}
+	//вводим биографию
 	cout << "Biography of your character: " << endl;
-	getline(cin, character._autobiography, ';');
+	getline(cin, character._autobiography, ';'); //из потока cin (т.е потока ввода) получаем строку, пока не встретим символ-разделитель (делиметр)
+	//и записываем эту строку в поле _autobography структуры Character
 	cout << "Biography:" << character._autobiography << endl;
-	character.ch_move = STOP;
-	character.ch_x = 2;
+	character.ch_move = STOP; //на старт игры не движимся 
+	character.ch_x = 2; // стоим в точке с координатами (2,2). ch_x - номер строки, ch_y - номер элемента в строке, порядковый номер которой ch_x
 	character.ch_y = 2;
-	DrawMap(character.ch_skin, current_map_name, character.ch_move);
+	DrawMap(character.ch_skin, current_map_name, character.ch_move); //отрисовываем карту
 	//cout << "Character: " << character._name << " Race: " << character._race << " Class: " <<character._class<< " Gender: " << character._gender << endl;
 }
 
 void DrawMap(char skin, string map_name, move_var ch_move)
 {
-	system("cls");//or cls
+	system("cls"); //читсим экран
 	string str{""};
-	if (map_change)
+	if (map_change)//если на карте произошли изменения (сдвинулся персонаж)
 	{
-		current_map.open(map_name);
-		if (!current_map.is_open())
-		{
+		current_map.open(map_name);//открываем файл с картой
+		if (!current_map.is_open())//проверяем открыт ли файл
+		{//если не котрыт - выводим сообщение об ошибке
 			cout << "Error. Map file " << map_name << " not open." << endl;
+			return; //выодим из функции
 		}
-		while (getline(current_map, str))
+		while (getline(current_map, str))// пока из потока current_map (потока ввода из файла (ifstream - input file stream))
+			//получаем строку (на этот раз без делиметра), записываем ее в переменную строкового типа str
 		{
-			current_draw_map.push_back(str);
+			current_draw_map.push_back(str); //в вектор строк current_draw_map в последний элемент (каждый элемент этого вектора - строка)
+			//записваем строку str. Цикл повторяется, пока в файле не кончатся строки. В итоге весь файл запишется в вектор строк (построчно).
 			//cout << current_draw_map.back()<<endl;
 		}
-		map_change = false;
-		height = current_draw_map.size(); //167 Х
-		width = current_draw_map.back().size();//38 Y
+		map_change = false; //возвращаем значение false флагу изменения карты. (чтобы отрисовывать карту только тогда, когда что то на нец изменилось)
+		height = current_draw_map.size(); //167 Х. Высота - количество элементов в векторе строк (т.е количество строк в файле)
+		width = current_draw_map.back().size();//38 Y. Ширина - количество символов в каждой строке.
 	/*cout << "width = " << width;
 	cout << "height = " << height;*/
 	}
+	//сама отрисовка карты. Пробегаем вектор строк поэлементно
 	for (int i = 0; i < height/*current_draw_map.size()*/; ++i)
 	{
+		//каждый элемент этого вектора(строку) так же пробегаем поэлементно
 		for (int j = 0; j < width/*current_draw_map.back().size()*/; ++j)
 		{
+			//находим координаты нашего персонажа (на старт игры - 2 строка, 2 элемент этой строки)
 			if ((i == character.ch_x) && (j == character.ch_y))
 			{
+				//заменяем этот элемент на символ персонажа
 				current_draw_map[i][j] = character.ch_skin;
+				//в предыдущее место нахождения персонажа рисуем пробел в зависимости от направления движения
 				switch (character.ch_move)
 				{
 				case LEFT: current_draw_map[i][j + 1] = ' '; break;
@@ -158,17 +170,21 @@ void DrawMap(char skin, string map_name, move_var ch_move)
 				}
 			}
 		}
+		//отрисовывем в консоль текущую строку вектора
 		cout << current_draw_map[i] << endl;
 	}
+	//закрываем файл карты (обязательно! иначен ошибка)
 	current_map.close();
+	//под карту пишем состояние персонажа
 	cout << "Character: " << character._name << " Race: " << character._race << " Class: " << character._class << " Gender: " << character._gender << endl;
 	cout << "Coordinates: " << character.ch_x << "	" << character.ch_y << endl;
 }
 
 void Input()
 {
+	//если на клавиатуре была нажата клавиша
 	if (_kbhit())
-	{
+	{//в зависимости от введенного символа выполняем следующее (меняем поле ch_move структуры Character)
 		switch (_getch())
 		{
 		case 'a': character.ch_move = LEFT; break;
@@ -182,6 +198,7 @@ void Input()
 
 void Logic()
 {
+	//в зависимости от текущего значения поля ch_move меняем координаты персонажа, вызываем функцию отрисовки карты
 	switch (character.ch_move)
 	{
 	case LEFT: if(character.ch_y > 1) --character.ch_y; DrawMap(character.ch_skin, current_map_name, character.ch_move); break;
@@ -189,5 +206,6 @@ void Logic()
 	case RIGHT: if(character.ch_y < width - 2) ++character.ch_y; DrawMap(character.ch_skin, current_map_name, character.ch_move); break;
 	case DOWN: if(character.ch_x < height - 2) ++character.ch_x; DrawMap(character.ch_skin, current_map_name, character.ch_move); break;
 	}
+	//отсанавливаем персонажа
 	character.ch_move = STOP;
 }
